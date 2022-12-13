e69024534f49a4c47f71b92d3f9b7a8e12e967e2
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-12-13 14:20:52 -0800

    changes to app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,12 @@ const app = express();
 const port = 8001;
 
 app.set('view engine', 'ejs');
+
 app.use(express.static('views'));
 
 app.get('/', function(req, res) {
   res.render('index');
 });
-
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
